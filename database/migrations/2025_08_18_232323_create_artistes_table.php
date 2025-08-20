4e71371bb6cb76f4fd25a8cf108764899e558bfa
Charles AGBO <agbo.charles85@gmail.com>
2025-08-21 00:46:40 +0200

    :bug:

--- database/migrations/2025_08_18_232323_create_artistes_table.php
+++ database/migrations/2025_08_18_232323_create_artistes_table.php
@@ -11,6 +11,10 @@ return new class() extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('artistes')) {
+            return;
+        }
+
         Schema::create('artistes', function (Blueprint $table) {
             $table->id();
             $table->string('name');

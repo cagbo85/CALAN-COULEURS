255c64015a2b90bef6dd9bd566e33a36211026eb
cagbo <agbo.charles85@gmail.com>
2025-11-12 11:20:56 +0100

    :wrench: chore/config - Make index.php path dynamic for local & production environments

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+$basePath = __DIR__;
+$laravelPath = is_dir($basePath . '/laravel')
+    ? $basePath . '/laravel'
+    : $basePath . '/../';
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = $laravelPath . '/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require $laravelPath . '/vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once $laravelPath . '/bootstrap/app.php';
+
+$app->handleRequest(Request::capture());

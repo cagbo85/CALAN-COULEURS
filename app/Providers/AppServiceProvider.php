29c18cbb9db5116927eebbcf2df2f9986ff8b36f
cagbo <agbo.charles85@gmail.com>
2026-03-20 21:43:41 +0100

    :zap: :wrench: :necktie: feat/scheduler - Auto-update edition statuses based on timeline

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Models\Edition;
 use App\Services\HelloAssoService;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Partager l'édition actuelle avec les vues d'erreur
+        View::composer('errors::*', function ($view) {
+            $view->with('currentEdition', Edition::getCurrentEdition());
+        });
+
+        View::composer('*', function ($view) {
+            $currentEdition = Edition::getCurrentEdition();
+
+            // Définir quels liens afficher selon le statut
+            $showProgrammation = $currentEdition && in_array($currentEdition->status, ['upcoming','ongoing', 'past']);
+            $showPartenaires = $currentEdition && in_array($currentEdition->status, ['upcoming','ongoing', 'past']);
+            // $showPhotoSouvenirs = $currentEdition && in_array($currentEdition->status, ['past']);
+            // $showNews = $currentEdition && in_array($currentEdition->status, ['upcoming','past']);
+
+            $view->with([
+                'currentEdition' => $currentEdition,
+                'showProgrammation' => $showProgrammation,
+                'showPartenaires' => $showPartenaires,
+                // 'showPhotoSouvenirs' => $showPhotoSouvenirs,
+                // 'showNews' => $showNews,
+            ]);
+        });
     }
 }

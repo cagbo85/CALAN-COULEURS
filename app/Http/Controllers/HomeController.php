6f97b2c073ac89f7a349f9a46bbae9823efc9afb
cagbo <agbo.charles85@gmail.com>
2026-04-07 18:38:21 +0200

    :fire: refactor/currentEdition - Removal of unnecessary code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,56 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Edition;
-
 class HomeController extends Controller
 {
     public function accueil()
     {
-        $edition = Edition::getCurrentEdition();
-
-        return view('accueil', ['edition' => $edition]);
+        return view('accueil');
     }
 
     public function festival()
     {
-        $edition = Edition::getCurrentEdition();
-
-        return view('festival', ['edition' => $edition]);
+        return view('festival');
     }
 
     public function benevoles()
     {
-        $edition = Edition::getCurrentEdition();
-
-        return view('benevoles', ['edition' => $edition]);
+        return view('benevoles');
     }
 
     public function camping()
     {
-        $edition = Edition::getCurrentEdition();
-
-        return view('camping', ['edition' => $edition]);
+        return view('camping');
     }
 
     public function charte()
     {
-        $edition = Edition::getCurrentEdition();
-
-        return view('charte', ['edition' => $edition]);
+        return view('charte');
     }
 
     public function contact()
     {
-        $edition = Edition::getCurrentEdition();
-
-        return view('contact', ['edition' => $edition]);
+        return view('contact');
     }
 
     public function partenaires()
     {
-        $edition = Edition::getCurrentEdition();
-
-        return view('partenaires', ['edition' => $edition]);
+        return view('partenaires');
     }
 }
